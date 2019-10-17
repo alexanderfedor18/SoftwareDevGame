17bcd277a3b5f19a84c8e75c7947f313bb20967d
alexanderfedor18 <alexanderfedor18@augustana.edu>
2019-10-16 20:57:12 -0500

    added string to determine what state shield is in for better hit detect

--- SoftwareDevGame/src/Shield.java
+++ SoftwareDevGame/src/Shield.java
@@ -6,34 +6,40 @@ public class Shield extends Entity{
 	private static final int baseWidth = 50;
 	private static final int baseHeight = 20;
 	
+	private String state;
 	private Game game;
 	
 	public Shield(Game game, float x, float y) {
 		super(x, y, baseWidth, baseHeight);
+		state = "up";
 		this.game = game;
 	}
 
 	@Override
 	public void tick() {
 		if (game.getKeyManager().up) {
+			state = "up";
 			super.setX(225);
 			super.setY(205);
 			super.setHeight(baseHeight);
 			super.setWidth(baseWidth);
 		}
 		if (game.getKeyManager().left) {
+			state = "left";
 			super.setX(205);
 			super.setY(225);
 			super.setHeight(baseWidth);
 			super.setWidth(baseHeight);
 		}
 		if (game.getKeyManager().down) {
+			state = "down";
 			super.setX(225);
 			super.setY(275);
 			super.setHeight(baseHeight);
 			super.setWidth(baseWidth);
 		}
 		if (game.getKeyManager().right) {
+			state = "right";
 			super.setX(275);
 			super.setY(225);
 			super.setHeight(baseWidth);
@@ -47,5 +53,9 @@ public class Shield extends Entity{
 		g.fillRect((int) x,(int) y, width, height);
 		
 	}
-
+	
+	public String getState() {
+		return state;
+	}
+	
 }
