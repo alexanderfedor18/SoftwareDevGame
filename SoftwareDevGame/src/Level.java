44c10633ec194ea4947b5bfa54cf0cb22ad1d6ac
alexanderfedor18 <alexanderfedor18@augustana.edu>
2019-10-24 11:55:26 -0500

    added janky functionality of pausing for spawning enemies

--- SoftwareDevGame/src/Level.java
+++ SoftwareDevGame/src/Level.java
@@ -3,6 +3,8 @@ import java.awt.Graphics;
 public abstract class Level {
 	public static Level currentLevel = null;
 	
+	private long lastEnemyTimer, enemyCooldown, enemyTimer;
+
 	
 
 	public Level(Game game, EntityManager entityManager) {
@@ -11,7 +13,17 @@ public abstract class Level {
 	}
 	
 	
+	protected Game game;
+	protected EntityManager entityManager;
 	
+	
+	public abstract void tick(); 
+	
+	public abstract void render(Graphics g);
+	
+	
+	//getters and setters
+
 	public static void setLevel(Level level) {
 		currentLevel = level;
 	}
@@ -20,11 +32,41 @@ public abstract class Level {
 		return currentLevel;
 	}
 	
-	protected Game game;
-	protected EntityManager entityManager;
 	
 	
-	public abstract void tick(); 
-	
-	public abstract void render(Graphics g);
+	public long getLastEnemyTimer() {
+		return lastEnemyTimer;
+	}
+
+
+
+	public void setLastEnemyTimer(long lastEnemyTimer) {
+		this.lastEnemyTimer = lastEnemyTimer;
+	}
+
+
+
+	public long getEnemyCooldown() {
+		return enemyCooldown;
+	}
+
+
+
+	public void setEnemyCooldown(long enemyCooldown) {
+		this.enemyCooldown = enemyCooldown;
+	}
+
+
+
+	public long getEnemyTimer() {
+		return enemyTimer;
+	}
+
+
+
+	public void setEnemyTimer(long enemyTimer) {
+		this.enemyTimer = enemyTimer;
+	}
+
+
 }
--- SoftwareDevGame/src/LevelOne.java
+++ SoftwareDevGame/src/LevelOne.java
@@ -5,27 +5,47 @@ public class LevelOne extends Level {
 	private final int ENEMY_SIZE = 30;
 	
 	
+	
+	
 	public LevelOne(Game game,EntityManager entityManager) {
 		super(game, entityManager);
+		this.setEnemyCooldown(800);
+		this.setEnemyTimer(this.getEnemyCooldown());
 		
 	}
-
+	
 	@Override
 	public void tick() {
-		if (game.getKeyManager().eU) {
-			entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "up"));
-		} else if (game.getKeyManager().eD) {
-			entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "down"));
-		} else if (game.getKeyManager().eL) {
-			entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "left"));
-		} else if (game.getKeyManager().eR) {
-			entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "right"));
-		}
+		
+		spawnEnemy();
 	}
 
 	@Override
 	public void render(Graphics g) {
 		
 	}
+	
+	
+	public void spawnEnemy() {
+		this.setEnemyTimer(this.getEnemyTimer() + System.currentTimeMillis() - this.getLastEnemyTimer());
+		this.setLastEnemyTimer(System.currentTimeMillis());
+		if (this.getEnemyTimer() < this.getEnemyCooldown()) {
+			return;
+		} else {
+			if (game.getKeyManager().eU) {
+				entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "up"));
+			} else if (game.getKeyManager().eD) {
+				entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "down"));
+			} else if (game.getKeyManager().eL) {
+				entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "left"));
+			} else if (game.getKeyManager().eR) {
+				entityManager.addEntity(new Enemy(0,0, ENEMY_SIZE, ENEMY_SIZE, "right"));
+			}
+
+			this.setEnemyTimer(0);
+	
+		}
+			
+			}
 
 }
